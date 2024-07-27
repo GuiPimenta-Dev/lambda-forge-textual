a68605d6f1c107cc2e5453f4530761636cbfa88a
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-07-27 18:56:11 -0300

    logs on second tab

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
 from textual.app import App, ComposeResult
-from textual.widgets import TabbedContent, Markdown
+from textual.widgets import TabbedContent, Markdown, Footer, TabPane
+from logs import Logs
 from server import Server
+from textual.binding import Binding
 
 ROWS = [
-    ("Name", "Service", "Type", "Trigger"),
     ("Live-Lambda-Forge-Demo-SecretAuthorizer", None, None, None),
     ("Live-Lambda-Forge-Demo-HelloWorld", "Api Gateway", "URL" ,"https://4hrnm139cf.execute-api.us-east-2.amazonaws.com/live/hello_world (GET)"),
-    ("Live-Lambda-Forge-Demo-HelloAndre", "SNS", "ARN" ,"arn:aws:sns:us-east-2:211125768252:andre_topic"),
+    ("Live-Lambda-Forge-Demo-HelloAndre", "SNS", "ARN" ,"arn:aws:sns:us-east-2:211125768252:andre_tqopic"),
 ]
 
 class Live(App):
     
-    CSS_PATH = "styles.tcss"
-    
+    BINDINGS = [
+        Binding("s", "show_tab('server')", "Server", show=False),
+        Binding("l", "show_tab('logs')", "Logs", show=False),
+        Binding("t", "show_tab('triggers')", "Trigger", show=False),
+    ]
     def __init__(self, rows, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.rows = rows
 
     def compose(self) -> ComposeResult:
-        with TabbedContent("Server", "Logs", "Triggers"):
-            yield Server(self.rows)
-            yield Markdown("Logs")
-            yield Markdown("Triggers")
-
-
+        yield Footer()
+        with TabbedContent():
+            
+            with TabPane("Server", id="server"):
+                yield Server(self.rows)
+            
+            with TabPane("Logs", id="logs"):
+                yield Logs("live.log")
+            
+            with TabPane("Triggers", id="triggers"):
+                yield Markdown("Triggers")
+                
+    def action_show_tab(self, tab: str) -> None:
+        """Switch to a new tab."""
+        self.get_child_by_type(TabbedContent).active = tab
 
 if __name__ == "__main__":
     Live(ROWS).run()
--- server.py
+++ server.py
@@ -1,24 +1,29 @@
 from rich.text import Text
 from textual.widgets import Static,DataTable
 from textual.app import ComposeResult
+from textual.widgets import RichLog, Footer
+from rich.table import Table
 
 
 class Server(Static):
+    
     def __init__(self, rows, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.rows = rows
 
     def compose(self) -> ComposeResult:
-        yield DataTable()
+        yield RichLog(highlight=True, markup=True, min_width=300)
 
     def on_mount(self) -> None:
-        table = self.query_one(DataTable)
-        table.cursor_type = "none"
-        styled_headers = [Text(header, style="bold green") for header in self.rows[0]]
-        table.add_columns(*styled_headers)
-
-        for row in self.rows[1:]:
+        text_log = self.query_one(RichLog)
+        
+        columns = ["Name", "Service", "Type", "Trigger"]
+        table = Table(*columns)
+        for row in self.rows:
             if None in row:
                 continue
-            styled_row = [Text(str(cell)) for cell in row]
-            table.add_row(*styled_row)
+            table.add_row(*row)
+        
+        text_log.write(table)
+
+        
