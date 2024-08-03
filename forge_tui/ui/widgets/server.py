61b141be9c181052ce8b5aefb9f28c415bee6e23
kraanzu <kraanzu@gmail.com>
2024-08-03 14:12:02 +0530

    feat: update server table widget

--- forge_tui/ui/widgets/server.py
+++ forge_tui/ui/widgets/server.py
@@ -1,4 +1,18 @@
+from textual.app import ComposeResult
 from textual.widgets import DataTable
+from textual.widget import Widget
 
-class ServerTable(DataTable):
-    pass
+
+class ServerTable(Widget):
+    COLUMNS = ["Name", "Service", "Type", "Trigger"]
+
+    @property
+    def table(self) -> DataTable:
+        return self.query_one(DataTable)
+
+    def on_mount(self) -> None:
+        for column in self.COLUMNS:
+            self.table.add_column(column)
+
+    def compose(self) -> ComposeResult:
+        yield DataTable(cursor_type="row")
