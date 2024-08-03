612c48a838113e342d6b929e35fa1d4e8f650bbd
kraanzu <kraanzu@gmail.com>
2024-08-03 14:21:27 +0530

    add `ForgeHeader` to widgets pkg

--- forge_tui/ui/widgets/__init__.py
+++ forge_tui/ui/widgets/__init__.py
@@ -1,6 +1,7 @@
 from .logs import LogStream
 from .server import ServerTable
 from .triggers import Triggers
+from .header import ForgeHeader
 
 
-__all__ = ["LogStream", "ServerTable", "Triggers"]
+__all__ = ["LogStream", "ServerTable", "Triggers", "ForgeHeader"]
