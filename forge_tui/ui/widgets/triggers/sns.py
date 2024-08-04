65c855ce28866ef2595ecda8a44e48ebbc7dbd46
kraanzu <kraanzu@gmail.com>
2024-08-04 19:09:55 +0530

    add sns trigger

--- forge_tui/ui/widgets/triggers/sns.py
+++ forge_tui/ui/widgets/triggers/sns.py
@@ -1,4 +1,29 @@
+from textual.app import ComposeResult
+from textual.widget import Widget
+from textual.widgets import Input, TextArea
 from ._base import TriggerBaseWidget
 
+
+class SNSContainer(Widget):
+    DEFAULT_CSS = """
+    SNSContainer {
+        layout: grid;
+        grid-size: 2 3;
+        grid-rows: 5 10 auto;
+        grid-columns: 1fr 1fr;
+
+        Input {
+            column-span: 2;
+        }
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        yield Input(id="topic_arn")
+        yield TextArea.code_editor(text="{}", language="json", id="message")
+        yield TextArea.code_editor(text="{}", language="json", id="subject")
+
+
 class SNS(TriggerBaseWidget):
-    pass
+    def render_left(self) -> ComposeResult:
+        yield SNSContainer()
