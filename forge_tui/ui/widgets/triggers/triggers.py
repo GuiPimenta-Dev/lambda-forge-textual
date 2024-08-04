afcfd387d47579ec80f72f86b687143e139c9f61
kraanzu <kraanzu@gmail.com>
2024-08-04 16:38:46 +0530

    add all triggers in triggers section

--- forge_tui/ui/widgets/triggers/triggers.py
+++ forge_tui/ui/widgets/triggers/triggers.py
@@ -1,4 +1,5 @@
-from textual.widget import Widget
+from textual.app import ComposeResult
+from textual.widgets import Static, TabPane, TabbedContent
 from .s3 import S3
 from .sns import SNS
 from .sqs import SQS
@@ -6,5 +7,21 @@ from .event_bridge import EventBridge
 from .api_gateway import ApiGateway
 
 
-class Triggers(Widget):
-    pass
+class Triggers(Static):
+
+    def compose(self) -> ComposeResult:
+        with TabbedContent():
+            with TabPane("Api Gateway", id="api_gateway"):
+                yield ApiGateway()
+
+            with TabPane("SNS", id="sns"):
+                yield SNS()
+
+            with TabPane("SQS", id="sqs"):
+                yield SQS()
+
+            with TabPane("S3", id="s3"):
+                yield S3()
+
+            with TabPane("EventBridge", id="event_bridge"):
+                yield EventBridge()
