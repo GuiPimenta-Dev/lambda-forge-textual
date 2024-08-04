fb484e56a22b86cc92b5b2f40c818f9be2595d1a
kraanzu <kraanzu@gmail.com>
2024-08-04 19:11:54 +0530

    add sqs trigger

--- forge_tui/ui/widgets/triggers/sqs.py
+++ forge_tui/ui/widgets/triggers/sqs.py
@@ -1,5 +1,24 @@
+from textual.app import ComposeResult
+from textual.widget import Widget
+from textual.widgets import Input, TextArea
 from ._base import TriggerBaseWidget
 
 
+class SQSContainer(Widget):
+    DEFAULT_CSS = """
+    SQSContainer {
+        layout: grid;
+        grid-size: 1 3;
+        grid-rows: 5 10 auto;
+        grid-columns: 1fr 1fr;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        yield Input(id="queue_url")
+        yield TextArea.code_editor(text="{}", language="json", id="message")
+
+
 class SQS(TriggerBaseWidget):
-    pass
+    def render_left(self) -> ComposeResult:
+        yield SQSContainer()
