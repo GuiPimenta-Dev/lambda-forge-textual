dec9df1b6e1831b815933ff21e8d2a630b655025
kraanzu <kraanzu@gmail.com>
2024-08-04 19:08:14 +0530

    add s3 trigger

--- forge_tui/ui/widgets/triggers/s3.py
+++ forge_tui/ui/widgets/triggers/s3.py
@@ -1,4 +1,29 @@
+from textual.app import ComposeResult
+from textual.widget import Widget
+from textual.widgets import Input, TextArea
 from ._base import TriggerBaseWidget
 
+
+class S3Container(Widget):
+    DEFAULT_CSS = """
+    S3Container {
+        layout: grid;
+        grid-size: 2 3;
+        grid-rows: 5 10 auto;
+        grid-columns: 1fr 1fr;
+
+        TextArea {
+            column-span: 2;
+        }
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        yield Input(id="bucket_name")
+        yield Input(id="file_path")
+        yield TextArea.code_editor(text="{}", language="json", id="metadata")
+
+
 class S3(TriggerBaseWidget):
-    pass
+    def render_left(self) -> ComposeResult:
+        yield S3Container()
