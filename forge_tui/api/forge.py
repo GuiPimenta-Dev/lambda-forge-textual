c599259d148b1deb39ac1667835eddf3acc5fda1
kraanzu <kraanzu@gmail.com>
2024-08-04 08:57:44 +0530

    use rich table instead of data table

--- forge_tui/api/forge.py
+++ forge_tui/api/forge.py
@@ -1,2 +1,21 @@
+from typing import List, Optional
+
+
 class Forge:
-    pass
+    def get_servers(self) -> List[List[Optional[str]]]:
+        ROWS = [
+            ("Live-Lambda-Forge-Demo-SecretAuthorizer", None, None, None),
+            (
+                "Live-Lambda-Forge-Demo-HelloWorld",
+                "Api Gateway",
+                "URL",
+                "https://4hrnm139cf.execute-api.us-east-2.amazonaws.com/live/hello_world (GET)",
+            ),
+            (
+                "Live-Lambda-Forge-Demo-HelloAndre",
+                "SNS",
+                "ARN",
+                "arn:aws:sns:us-east-2:211125768252:andre_tqopic",
+            ),
+        ]
+        return ROWS
--- forge_tui/ui/widgets/server.py
+++ forge_tui/ui/widgets/server.py
@@ -1,6 +1,13 @@
-from textual.app import ComposeResult
-from textual.widgets import DataTable
+from rich.console import RenderableType
 from textual.widget import Widget
+from rich.table import Table
+from forge_tui.api import Forge
+
+# from textual.app import ComposeResult, on
+# from textual.events import Resize
+# from textual.widgets import DataTable
+
+forge = Forge()
 
 
 class ServerTable(Widget):
@@ -8,17 +15,45 @@ class ServerTable(Widget):
     ServerTable {
         height: auto;
     }
+
+    ServerTable > DataTable {
+        width: 50;
+    }
     """
 
     COLUMNS = ["Name", "Service", "Type", "Trigger"]
+    RATIOS = [None, None, None, 1]
 
-    @property
-    def table(self) -> DataTable:
-        return self.query_one(DataTable)
+    def render(self) -> RenderableType:
 
-    def on_mount(self) -> None:
-        for column in self.COLUMNS:
-            self.table.add_column(column)
+        table = Table(expand=True)
+        table.show_lines = True
 
-    def compose(self) -> ComposeResult:
-        yield DataTable(cursor_type="row")
+        for column, ratio in zip(self.COLUMNS, self.RATIOS):
+            table.add_column(column, ratio=ratio)
+
+        for row in forge.get_servers():
+            table.add_row(*row)
+
+        return table
+
+    # @on(Resize)
+    # def _resize(self, event: Resize) -> None:
+    #     self.refresh_table()
+    #
+    # @property
+    # def table(self) -> DataTable:
+    #     return self.query_one(DataTable)
+    #
+    # def refresh_table(self):
+    #     width = self.size.width
+    #     for column in self.COLUMNS:
+    #         self.table.add_column(column, width=width // len(self.COLUMNS))
+    #
+    #     self.refresh()
+    #
+    # def on_mount(self) -> None:
+    #     self.refresh_table()
+    #
+    # def compose(self) -> ComposeResult:
+    #     yield DataTable(cursor_type="row")
