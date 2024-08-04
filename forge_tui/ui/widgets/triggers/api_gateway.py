5df0876ac291f23eb136a9639640d025037dda97
kraanzu <kraanzu@gmail.com>
2024-08-04 16:38:54 +0530

    add api gateway basic

--- forge_tui/ui/widgets/triggers/api_gateway.py
+++ forge_tui/ui/widgets/triggers/api_gateway.py
@@ -1,4 +1,26 @@
+from textual.app import ComposeResult
+from textual.widgets import Input, Select, TextArea
 from ._base import TriggerBaseWidget
 
+
 class ApiGateway(TriggerBaseWidget):
-    pass
+    DEFAULT_CSS = """
+    ApiGateway {
+        layout: grid;
+        grid-size: 3 3;
+        grid-rows: 5 10 auto;
+    }
+
+    ApiGateway > #url {
+        column-span: 2;
+    }
+    """
+
+    METHODS = ["GET", "POST", "PUT", "DELETE", "PATCH"]
+
+    def compose(self) -> ComposeResult:
+        yield Select(options=[(i, i) for i in self.METHODS], id="method")
+        yield Input(id="url")
+        yield TextArea.code_editor(text="{}", language="json", id="query")
+        yield TextArea.code_editor(text="{}", language="json", id="body")
+        yield TextArea.code_editor(text="{}", language="json", id="headers")
