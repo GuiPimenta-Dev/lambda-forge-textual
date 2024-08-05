e558ee65b9457ca61dc7d4091d620bc7360e615f
kraanzu <kraanzu@gmail.com>
2024-08-05 08:50:57 +0530

    add method to get values

--- forge_tui/ui/widgets/triggers/_base.py
+++ forge_tui/ui/widgets/triggers/_base.py
@@ -1,5 +1,6 @@
+from typing import Dict
 from textual.app import ComposeResult
-from textual.widgets import Static
+from textual.widgets import Input, Select, Static, TextArea
 from ._result_window import ResultWindow
 
 
@@ -15,6 +16,24 @@ class TriggerBaseWidget(Static):
 
     def render_left(self) -> ComposeResult:
         yield from []
+
+    def get_input_values(self) -> Dict:
+        data = {}
+
+        for widget in self.children:
+            if not widget.id:
+                continue
+
+            _id = widget.id
+
+            if isinstance(widget, Input):
+                data[_id] = widget.value
+            elif isinstance(widget, TextArea):
+                data[_id] = widget.text
+            elif isinstance(widget, Select):
+                data[_id] = widget.value
+
+        return data
 
     def compose(self) -> ComposeResult:
         yield from self.render_left()
