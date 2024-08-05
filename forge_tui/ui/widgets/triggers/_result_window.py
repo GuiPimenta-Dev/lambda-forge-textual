e08234b7415fb9e9c097d7c09bca927b57795ffd
kraanzu <kraanzu@gmail.com>
2024-08-05 21:50:25 +0530

    add RunHistoryItem

--- forge_tui/ui/widgets/triggers/_result_window.py
+++ forge_tui/ui/widgets/triggers/_result_window.py
@@ -1,6 +1,13 @@
 from textual.app import ComposeResult
 from textual.widget import Widget
 from textual.widgets import OptionList, Static
+from textual.widgets.option_list import Option
+
+
+class RunHistoryItem(Option):
+    def __init__(self, history):
+        super().__init__(str(history))
+        self.history = history
 
 
 class ResultWindow(Widget):
@@ -15,6 +22,13 @@ class ResultWindow(Widget):
     }
     """
 
+    @property
+    def history_list(self) -> OptionList:
+        return self.query_one(OptionList)
+
     def compose(self) -> ComposeResult:
         yield OptionList()
         yield Static()
+
+    def add_history(self, history):
+        self.history_list.add_option(RunHistoryItem(history))
