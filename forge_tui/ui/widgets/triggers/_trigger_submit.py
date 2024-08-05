2d6bff49873af1cfcf7eb0708cb56d4be610ed6b
kraanzu <kraanzu@gmail.com>
2024-08-05 21:54:47 +0530

    add history when user submits

--- forge_tui/ui/widgets/triggers/_trigger_submit.py
+++ forge_tui/ui/widgets/triggers/_trigger_submit.py
@@ -4,4 +4,17 @@ from textual.widgets import Button
 class TriggerSubmit(Button):
 
     def on_click(self):
-        pass
+        if not self.parent:
+            raise ValueError("TriggerSubmit must be a child of a form")
+
+        from ._base import TriggerBaseWidget
+
+        if not isinstance(self.parent, TriggerBaseWidget):
+            raise ValueError("Container not found")
+
+        from ._result_window import ResultWindow
+
+        res = self.parent.query_one(ResultWindow)
+        values = self.parent.get_input_values()
+
+        res.add_history(values)
