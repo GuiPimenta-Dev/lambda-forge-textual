eb352b30e38909f6c03af755a20a4b619ab96b4b
kraanzu <kraanzu@gmail.com>
2024-08-06 17:34:34 +0530

    synthesis timer

--- forge_tui/ui/screens/loading.py
+++ forge_tui/ui/screens/loading.py
@@ -1,19 +1,30 @@
 from textual import work
+from textual.app import ComposeResult
 from textual.screen import Screen
+from textual.widgets import LoadingIndicator
 
 
 class LoadingScreen(Screen):
 
     async def on_mount(self):
+        self.is_syntesis_complete = False
         self.init_forge()
-        self.push_main_screen()
+        self.set_interval(0.1, self.check_for_synthesis)
 
-    @work
+    @work(thread=True)
     async def init_forge(self):
         from time import sleep
 
-        sleep(5)
+        sleep(2)
+        self.is_syntesis_complete = True
 
-    def push_main_screen(self):
+    def check_for_synthesis(self):
+        if self.is_syntesis_complete:
+            self.dismiss_screen()
+
+    def dismiss_screen(self):
         self.app.pop_screen()
         self.app.push_screen("index")
+
+    def compose(self) -> ComposeResult:
+        yield LoadingIndicator()
