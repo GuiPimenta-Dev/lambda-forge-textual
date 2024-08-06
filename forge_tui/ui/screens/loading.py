d2016ecac5cc324101ecf5cf954ccfafb9c53598
kraanzu <kraanzu@gmail.com>
2024-08-06 17:19:28 +0530

    add 5 sec delay

--- forge_tui/ui/screens/loading.py
+++ forge_tui/ui/screens/loading.py
@@ -1,5 +1,19 @@
+from textual import work
 from textual.screen import Screen
 
 
 class LoadingScreen(Screen):
-    pass
+
+    async def on_mount(self):
+        self.init_forge()
+        self.push_main_screen()
+
+    @work
+    async def init_forge(self):
+        from time import sleep
+
+        sleep(5)
+
+    def push_main_screen(self):
+        self.app.pop_screen()
+        self.app.push_screen("index")
--- forge_tui/ui/tui.py
+++ forge_tui/ui/tui.py
@@ -1,12 +1,13 @@
 from textual.app import App
-from .screens import IndexScreen
+from .screens import IndexScreen, LoadingScreen
 
 
 class ForgeTUI(App):
     SCREENS = {
         "index": IndexScreen(),
+        "loader": LoadingScreen(),
     }
     CSS_PATH = "styles.css"
 
     def on_mount(self) -> None:
-        self.push_screen("index")
+        self.push_screen("loader")
